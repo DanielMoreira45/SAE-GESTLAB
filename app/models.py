08e06c9a4b52d10218dfcf0c41f14935c5c3f3d2
Yohann Loiseau <yohann.loiseau@etu.univ-orleans.fr>
2023-10-24 16:56:36 +0200

    début de la connexion

--- app/models.py
+++ app/models.py
@@ -1,1 +1,6 @@
 """Lien avec la Base de données"""
+from .app import db
+from flask_login import UserMixin
+
+class Utilisateur():
+    ...
--- app/views.py
+++ app/views.py
@@ -1,1 +1,42 @@
 """Toute les routes et les Formulaires"""
+from .app import app
+from flask import render_template, url_for, redirect, request
+from .models import Utilisateur    
+from flask_login import login_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, HiddenField, PasswordField
+from hashlib import sha256
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    next = HiddenField()
+
+    def get_authenticated_user(self):
+        user = Utilisateur.query.get(self.username.data)
+        if user is None:
+            return None
+        m = sha256()
+        m.update(self.password.data.encode())
+        passwd = m.hexdigest()
+        return user if passwd == user.password else None
+
+
+
+@app.route("/", methods=("GET","POST",))
+def login():
+    print("login")
+    f = LoginForm()
+    if not f.validate_on_submit():
+        f.next.data = request.args.get("next")
+    elif f.validate_on_submit():
+        user = f.get_authenticated_user()
+        if user:
+            login_user(user)
+            next = f.next.data or url_for("home")
+            return redirect(next)
+    return render_template(
+        "connexion.html",
+        form=f
+    )
