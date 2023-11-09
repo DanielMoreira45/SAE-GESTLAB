208a33a244bcd3b53a4f97e8c86885c878b813c0
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2023-11-09 11:17:06 +0100

    <fix>: Utilisation des validateurs pour vérifier que l'utilisateur n'a pas choisi l'option par défault du SelectField

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from .models import Utilisateur, Materiel, Commande, Commander, get_liste_materi
 from flask_login import login_required, login_user, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, HiddenField, PasswordField, SelectField, RadioField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, NoneOf
 from hashlib import sha256
 from datetime import datetime
 
@@ -82,45 +82,39 @@ def delivery():
     return None #TODO
 
 class CommandeForm(FlaskForm):
-    msg = ""
     with app.app_context():
         choix_materiel = get_liste_materiel()
         choix_materiel.insert(0, ("", "-- Choisir le matériel --"))
-        materiel_field = SelectField('Matériel', choices=choix_materiel, default="")
+        materiel_field = SelectField('Matériel', choices=choix_materiel, validators=[DataRequired("Merci de sélectionner une option.")], default="default")
         quantity_field = IntegerField("Quantité", validators=[DataRequired(), NumberRange(1, 1000)], default=1)
 
 @app.route("/delivery/new/")
 @login_required
 def new_commande():
     f = CommandeForm()
-    return render_template("new_commande.html", form=f, id_msg="", msg="")
+    return render_template("new_commande.html", form=f)
 
 @app.route("/delivery/new/save", methods=("POST",))
 def save_new_commande():
     f = CommandeForm()
-    if f.materiel_field.data != "":
-        commande = Commande(
-            numero = 1 + db.session.query(db.func.max(Commande.numero)).scalar(),
-            date_commande = datetime.utcnow(),
-            date_reception = None,
-            statut = "Non validée",
-            id_util = current_user.id,
-            ref_materiel = f.materiel_field.data[0]
-        )
-        commander = Commander(
-            numero_commande = 1 + db.session.query(db.func.max(Commander.numero_commande)).scalar(),
-            quantite_commandee = f.quantity_field.data,
-            id_util = current_user.id,
-            ref_materiel = f.materiel_field.data
-        )
-        db.session.add(commande)
-        db.session.commit()
-        db.session.add(commander)
-        db.session.commit()
-    else:
-        f.msg = "Merci de sélectionner une option."
-        # return render_template("new_commande.html", form=f, id_msg="error", msg="Merci de sélectionner une option.")
-        # pass
+    commande = Commande(
+        numero = 1 + db.session.query(db.func.max(Commande.numero)).scalar(),
+        date_commande = datetime.utcnow(),
+        date_reception = None,
+        statut = "Non validée",
+        id_util = current_user.id,
+        ref_materiel = f.materiel_field.data
+    )
+    commander = Commander(
+        numero_commande = 1 + db.session.query(db.func.max(Commander.numero_commande)).scalar(),
+        quantite_commandee = f.quantity_field.data,
+        id_util = current_user.id,
+        ref_materiel = f.materiel_field.data
+    )
+    db.session.add(commande)
+    # db.session.commit()
+    db.session.add(commander)
+    db.session.commit()
     return redirect(url_for('new_commande'))
 
 @app.route("/admin/home/")
