bad09a1c52303a0e2a1c1961714f56ffb79555ef
CassandraM <114163684+Plumatachi@users.noreply.github.com>
2023-12-18 08:35:46 +0100

    Fichier forms.py créé

--- app/views.py
+++ app/views.py
@@ -1,51 +1,11 @@
 """Toute les routes et les Formulaires"""
 from .app import app, db
 from .models import Materiel, Utilisateur, Domaine, Categorie, Role, Commande , filter_commands
-
+from .forms import LoginForm, UtilisateurForm, UserForm, CommandeForm
 from flask import jsonify, render_template, url_for, redirect, request, flash
 from flask_login import login_required, login_user, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField, PasswordField, SelectField, RadioField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
 from hashlib import sha256
 from datetime import datetime
-
-class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
-    password_incorrect = ""
-    next = HiddenField()
-
-    def get_authenticated_user(self):
-        user = Utilisateur.query.filter_by(email=self.email.data).first()
-        if user and user.password == self.password.data:
-            return user
-    
-    def has_content(self):
-        return self.password.data != "" or self.email.data != ""
-    
-    def show_password_incorrect(self):
-        self.password_incorrect = "Email ou mot de passe incorrect"
-
-# Permet la modification de l'utilisateur
-class UserForm(FlaskForm):
-    id = HiddenField('id')
-    nom = StringField('nom', validators=[DataRequired()])
-    prenom = StringField('prenom', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    id_role = SelectField('role', validators=[DataRequired()], choices=[(1, 'Administrateur'), (2, 'Professeur'), (3, 'Etablissement')])
-    modifications = RadioField('modifications', validators=[DataRequired()])
-
-# Permet l'insertion de l'utilisateur
-class UtilisateurForm(FlaskForm):
-    idUti = HiddenField('iduti')
-    idRole = HiddenField('idrole')
-    nomUti = StringField('Nom', validators=[DataRequired()])
-    prenomUti = StringField('Prénom', validators=[DataRequired()])
-    emailUti = StringField('Email', validators=[DataRequired()])
-    mdp = PasswordField('Mot de Passe', validators=[DataRequired()])
-    role = SelectField('Rôle', choices=[(1, 'Administrateur'), (2, 'Professeur'), (3, 'Etablissement')])
-    modif = RadioField('Droit de Modification', choices=[(True, 'Oui'), (False, 'Non')], validators=[DataRequired()])
 
 @app.route("/")
 def home():
@@ -227,11 +187,6 @@ def validate(validee, id):
         commande.statut = "Annulée"
     db.session.commit()
     return jsonify({'id':id})
-
-
-class CommandeForm(FlaskForm):
-    materiel_field = SelectField('Matériel', validators=[DataRequired("Merci de sélectionner une option.")])
-    quantity_field = IntegerField("Quantité", validators=[DataRequired(), NumberRange(1, 1000)], default=1)
 
 @app.route("/delivery/new/")
 @login_required
