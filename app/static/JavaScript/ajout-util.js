ed691f8c0033ae65c0ea8692284d62dad1db5fb6
CassandraM <114163684+Plumatachi@users.noreply.github.com>
2024-02-19 10:20:54 +0100

    Mot de passe temporaire

--- app/static/JavaScript/ajout-util.js
+++ app/static/JavaScript/ajout-util.js
@@ -1,7 +1,17 @@
+function createPassword(nbCaractere, chaine="azertyuiopqsdfghjklmwxcvbn123456789"){
+    var nbLettres = chaine.length + 1;
+    var passwd = "";
+    for (var i=0; i<nbCaractere; i++){
+        var positionLettre = Math.round(Math.random()*nbLettres);
+        var lettre = chaine[positionLettre];
+        passwd += lettre;
+    }
+    return passwd;
+}
+
 function sendEmail(){
     emailjs.init("FLCWfGzjhSIMLmML1");
     var emailUti = document.getElementById("emailUti");
-    var mdpUti = document.getElementById("mdp");
-    emailjs.send("service_kvw3ho3", "template_5okli8n", {email: emailUti.value, mdp: mdpUti.value})
+    emailjs.send("service_kvw3ho3", "template_5okli8n", {email: emailUti.value, mdp: createPassword(8)})
     .then(alert("Email envoyé avec succès"));
 }
